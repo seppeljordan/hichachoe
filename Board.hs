dd5c15a52f3254731cebbfb3a0ab0f9c6467acb2
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2014-06-22 02:38:36 +0200

    Reimplement winning conditions
    Board.hs
    * rename lineLength to boardDimensions for more consistent naming
      scheme
    * implement firstPlayer constructor to make it possible to start a
      game without knowing the actual implementation of the Player type

--- Board.hs
+++ Board.hs
@@ -1,20 +1,22 @@
 -- This module implements the the logic of the game board of a tic tac
 -- toe game
 
-module Board ( Board, makeEmptyBoard, boardFieldAt
-             , Player , otherPlayer
+module Board ( Board, makeEmptyBoard, boardFieldAt, boardChangeField
+             , boardDimensions, boardEmptyAt, boardPlayerAt, boardWon
+             , Player , otherPlayer, firstPlayer
              , Coordinates, validCoordinates
              , Field )
 where
 
-lineLength = 3
+boardDimensions :: Int
+boardDimensions = 3
 
 type Coordinates = (Int,Int)
 
 validCoordinates :: Coordinates -> Bool
 validCoordinates (x,y)
-    | x < 0 || x > lineLength = False
-    | y < 0 || y > lineLength = False
+    | x < 0 || x > boardDimensions = False
+    | y < 0 || y > boardDimensions = False
     | otherwise = True
 
 data Field = Empty | X | O deriving (Eq)
@@ -39,7 +41,7 @@ lineSetFieldAt l n newElem = makeLine $ iterateLine (getLineList l) n
     where iterateLine l 0 = newElem:(tail l)
           iterateLine l n = (head l):(iterateLine (tail l) (n - 1))
 
-makeNewLine = makeLine $ take lineLength $ repeat Empty
+makeNewLine = makeLine $ take boardDimensions $ repeat Empty
 
 instance Show Line where
     show l = concat (map show (getLineList l))
@@ -49,42 +51,78 @@ data Board = Board [Line] deriving (Eq)
 makeBoardOfList lines = Board lines
 boardLines (Board lines) = lines
 
-changeBoardField :: Board -> Coordinates -> Field -> Board
-changeBoardField board (x,y) newValue = makeBoardOfList $ iterateBoard (boardLines board) y
+changeBoardField :: Board -> Coordinates -> Player -> Board
+changeBoardField board (x,y) player = makeBoardOfList $ iterateBoard (boardLines board) y
     where iterateBoard (line:lines) 0 = (lineSetFieldAt line x newValue):lines
           iterateBoard (line:lines) n = line:(iterateBoard lines (n - 1))
+          newValue = playerToField player
 
 boardChangeField = changeBoardField
 
 boardEmptyAt :: Board -> Coordinates -> Bool
 boardEmptyAt b (x,y)
     | not (validCoordinates (x,y)) = error $ "boardEmptyAt: invalid Coordinates " ++ show (x,y)
-    | boardFieldAt b (x,y) == Just Empty = True
+    | boardFieldAt b (x,y) == Empty = True
     | otherwise = False
 
 makeEmptyBoard :: Board
-makeEmptyBoard = makeBoardOfList (take lineLength $ repeat makeNewLine)
+makeEmptyBoard = makeBoardOfList (take boardDimensions $ repeat makeNewLine)
 
 
 instance Show Board where
     show b = unlines $ map show $ map getLineList (boardLines b)
 
-boardFieldAt :: Board -> Coordinates -> Maybe Field
+boardFieldAt :: Board -> Coordinates -> Field
 boardFieldAt board (x,y)
-    | validCoordinates (x,y) = Just (lineFieldAt (line y) x)
+    | validCoordinates (x,y) = (lineFieldAt (line y) x)
     | otherwise = error $ "boardFieldAt: invalid coordinates " ++ show (x,y)
     where line n = (boardLines board) !! n
 
+boardPlayerAt :: Board -> Coordinates -> Maybe Player
+boardPlayerAt b (x,y)
+    | boardFieldAt b (x,y) == Empty = Nothing
+    | otherwise = Just $ fieldToPlayer $ boardFieldAt b (x,y)
+
+boardWon :: Board -> Maybe Player
+boardWon b
+    | playerWon PlayerOne && playerWon PlayerTwo = error $ "Invalid game situation:\n" ++ show b
+    | playerWon PlayerOne = Just PlayerOne
+    | playerWon PlayerTwo = Just PlayerTwo
+    | otherwise = Nothing
+    where boardList :: [Maybe Player]
+          boardList = map (boardPlayerAt b) [ (x,y) | y <- [0..boardMaxIndex], x <- [0..boardMaxIndex] ]
+          boardMaxIndex :: Int
+          boardMaxIndex = boardDimensions - 1
+          boardSquare :: [[Maybe Player]]
+          boardSquare = let aux [] accu = accu
+                            aux xs accu = aux (drop boardDimensions xs) ((take boardDimensions xs):accu)
+                        in aux boardList [[]]
+          has :: Player -> Int -> (Int -> [Maybe Player]) -> Bool
+          has p n line = foldl (&&) True $ map ((Just p) ==) (line n)
+          row :: Int -> [Maybe Player]
+          row n = boardSquare !! n
+          col :: Int -> [Maybe Player]
+          col n = [(boardSquare !! n) !! y  | y <- [0..boardMaxIndex]]
+          diag :: Int -> [Maybe Player]
+          diag 0 = [(boardSquare !! n) !! n | n <- [0..boardMaxIndex]]
+          diag 1 = [(boardSquare !! n) !! (boardMaxIndex - n) | n <- [0..boardMaxIndex]]
+          playerWon :: Player -> Bool
+          playerWon p = or $ [ has p r row | r <- [0..boardMaxIndex]]
+                        ++ [ has p r col | r <- [0..boardMaxIndex]]
+                        ++ [ has p r diag | r <- [0,1]]
 
 boardToString n [] = ""
 boardToString n (b:bs)
-    | n == (lineLength - 1) = (fieldToString b) ++ "\n" ++ boardToString 0 bs
+    | n == (boardDimensions - 1) = (fieldToString b) ++ "\n" ++ boardToString 0 bs
     | otherwise = (fieldToString b) ++ boardToString (n + 1) bs
 
 data Player = PlayerOne | PlayerTwo | PlayerNone deriving (Eq)
 
 instance Show Player where
     show = playerToString
+
+firstPlayer :: Player
+firstPlayer = PlayerOne
 
 playerToField PlayerOne = X
 playerToField PlayerTwo = O
